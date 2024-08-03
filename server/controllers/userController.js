8584189d0517b60d7f8b8fd7764425b1fd9160db
alermlv <102925147+alermlv@users.noreply.github.com>
2024-08-03 10:59:52 +0300

    npm i jsonwebtoken, bcrypt, create registration, login, delete user logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,53 @@
 const ApiError = require("../error/ApiError");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { User, Password } = require("../models/models");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign(
+    { id, email, role }, 
+    process.env.SECRET_KEY, 
+    { expiresIn: '24h' }
+  )
+}
 
 class UserController {
-  async registration(req, res) {
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
 
+    if (!email || !password) {
+      return next(ApiError.badRequest("Неправильный email или пароль!"))
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+
+    if (candidate) {
+      return next(ApiError.badRequest("Пользователь с таким email уже существует!"))
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, password: hashPassword, role });
+    const token = await generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
   }
 
-  async login(req, res) {
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
 
+    if (!user) {
+      return next(ApiError.badRequest("Пользователь с таким email не существует!"))
+    }
+
+    const comparePassword = bcrypt.compareSync(password, user.password);
+
+    if (!comparePassword) {
+      return next(ApiError.badRequest("Введен неверный пароль!"))
+    }
+
+    const token = await generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
   }
 
   async auth(req, res) {
@@ -14,7 +55,10 @@ class UserController {
   }
 
   async delete(req, res) {
-
+    const { id } = req.params;
+    await Password.destroy({ where: { userId: id } });
+    await User.destroy({ where: { id } });
+    res.status(200).json();
   }
 }
 
